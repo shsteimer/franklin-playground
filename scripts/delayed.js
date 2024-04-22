38582f209a6d18a74656dfba44a08fe48ccd2559
Sean Steimer <ssteimer@adobe.com>
2024-04-22 09:56:05 -0700

    changes to force tbt in delayed

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,4 +5,7 @@ import { sampleRUM, loadScript } from './aem.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
-loadScript('https://code.jquery.com/jquery-3.7.1.js');
+loadScript('https://code.jquery.com/jquery-2.2.4.js').then(() => {
+  const h1 = document.querySelector('h1');
+  h1.textContent += ': JQ 2.2.4 loaded!';
+});
